0a09c08e4dec1daf4f573abafa389529a5ad78b9
kokgergo <kok.gergo@gmail.com>
2023-09-23 13:40:59 +0200

    Automapper and repositories

---
+++ HotelListing.API/Models/Country/UpdateCountryDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.Country
+{
+    public class UpdateCountryDto : BaseCountryDto
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+
+}
